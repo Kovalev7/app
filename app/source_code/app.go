b855e166f6159b71c54bf737b5792ff9bac75e45
root <root@vm-deb2.ru-central1.internal>
2023-08-31 16:08:30 +0000

    create project

---
+++ app/source_code/app.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	ctx := context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "51.250.103.230:6379",
+		Password: "", // no password docs
+		DB:       0,  // use default DB
+	})
+
+
+
+	err := rdb.Set(ctx, "bike:1", "Process 134", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("OK")
+
+	value, err := rdb.Get(ctx, "bike:1").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("The name of the bike is %s", value)
+
+}
---
+++ app/source_code/go.mod
@@ -0,0 +1,5 @@
+module github.com/Kovalev7/app
+
+go 1.15
+
+require github.com/redis/go-redis/v9 v9.1.0 // indirect
